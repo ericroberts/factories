a5cfe27023a00e3c7d55d33334f1bd248da71947
Eric Roberts <ericroberts@gmail.com>
2015-04-21 15:11:53 -0400

    Implement estimated advance on customer.

--- example/lib/customer.rb
+++ example/lib/customer.rb
@@ -3,4 +3,11 @@ require_relative "../config/environment"
 class Customer < ActiveRecord::Base
   belongs_to :rate
   belongs_to :estimator
+
+  def estimated_advance
+    [
+      revenue * rate.min,
+      revenue * rate.max
+    ]
+  end
 end
--- example/spec/estimator_spec.rb
+++ example/spec/estimator_spec.rb
@@ -9,19 +9,17 @@ RSpec.describe Estimator do
   describe "#advance" do
     let(:customer) { double }
     let(:rate) { double }
+    let(:estimated_advance) { [80,90] }
 
     before do
       allow(subject).to receive(:customers).and_return([customer])
-      allow(customer).to receive(:revenue).and_return(100)
-      allow(customer).to receive(:rate).and_return(rate)
-      allow(rate).to receive(:min).and_return(80)
-      allow(rate).to receive(:max).and_return(90)
+      allow(customer).to receive(:estimated_advance).and_return(estimated_advance)
     end
 
     it "should return the sum of the estimated min and max advances" do
       advance = subject.advance
-      expect(advance.min).to eq customer.revenue * customer.rate.min
-      expect(advance.max).to eq customer.revenue * customer.rate.max
+      expect(advance.min).to eq customer.estimated_advance.min
+      expect(advance.max).to eq customer.estimated_advance.max
     end
   end
 end

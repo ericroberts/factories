1d3360ae906a4c790104e7e6d5c678df72d80a73
Eric Roberts <ericroberts@gmail.com>
2015-04-21 10:52:55 -0400

    Change tests to not use factories at all.

--- example/spec/estimator_spec.rb
+++ example/spec/estimator_spec.rb
@@ -4,10 +4,20 @@ require "customer"
 require "rate"
 
 RSpec.describe Estimator do
-  subject { customer.estimator }
-  let(:customer) { create :customer }
+  subject { Estimator.new }
 
   describe "#projection" do
+    let(:customer) { double }
+    let(:rate) { double }
+
+    before do
+      allow(subject).to receive(:customers).and_return([customer])
+      allow(customer).to receive(:revenue).and_return(100)
+      allow(customer).to receive(:rate).and_return(rate)
+      allow(rate).to receive(:min).and_return(80)
+      allow(rate).to receive(:max).and_return(90)
+    end
+
     it "should return the sum of the estimated min and max projections" do
       projection = subject.projection
       expect(projection.min).to eq customer.revenue * customer.rate.min

ebe4fcbe2bf03f90db829516ce3619787353ece6
Eric Roberts <ericroberts@gmail.com>
2015-04-21 15:11:53 -0400

    Implement projection method on customer.

--- example/lib/customer.rb
+++ example/lib/customer.rb
@@ -3,4 +3,11 @@ require_relative "../config/environment"
 class Customer < ActiveRecord::Base
   belongs_to :rate
   belongs_to :estimator
+
+  def projection
+    [
+      revenue * rate.min,
+      revenue * rate.max
+    ]
+  end
 end

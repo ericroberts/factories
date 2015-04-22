4809e5b30442894b9c53520eb432e6a001e19f2b
Eric Roberts <ericroberts@gmail.com>
2015-04-22 19:10:22 -0400

    More succinct projection method.
    Now that we’ve abstracted everything away we can take advantage of
    some nice Ruby features to write less code.

--- example/lib/estimator.rb
+++ example/lib/estimator.rb
@@ -5,8 +5,6 @@ class Estimator < ActiveRecord::Base
   has_many :customers
 
   def projection
-    customers.inject(MinMax.zero) do |minmax, customer|
-      minmax + customer.projection
-    end
+    customers.map(&:projection).inject(MinMax.zero, :+)
   end
 end
